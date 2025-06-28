5ac79ab84c7807c5f39352c331a9c75bd903e511
Himanshu <pinghimanshutiwari@gmail.com>
2025-06-28 21:36:38 +0530

    Initial commit - votrust

---
+++ src/main/java/com/facepass/votrust/VotrustBackendApplication.java
@@ -0,0 +1,13 @@
+package com.facepass.votrust;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotrustBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VotrustBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/facepass/votrust/VotrustBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.facepass.votrust;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotrustBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
